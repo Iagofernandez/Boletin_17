ebfe51b347bb5f07638f3315b8e71c0f49fcddb5
ifernandezblanco <ifernandezblanco@nautilus30.danielcastelao.org>
2019-01-29 09:02:35 +0100

    Boletin_17

---
+++ Boletin_17/src/boletin_17/Boletin_17.java
@@ -0,0 +1,24 @@
+
+package boletin_17;
+
+/**
+ *
+ * @author ifernandezblanco
+ */
+public class Boletin_17 {
+
+  
+    public static void main(String[] args) {
+      Gato obxG=new Gato();
+        obxG.nadar();
+        obxG.caminhar();
+        
+        Mamiferos obxM=new Mamiferos();
+        obxM.caminhar();
+        
+        Papagaio obxP=new Papagaio();
+        obxP.caminhar();
+    }
+    
+}
+ 
